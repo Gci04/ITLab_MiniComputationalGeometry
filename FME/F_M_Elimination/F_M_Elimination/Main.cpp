1eaa189f6572c8ed699fab342670bb7607f50f19
Gci04 <gcinzoe04@gmail.com>
2016-02-02 14:49:22 +0300

    First Chernikov rule
    - Created a class to store the indexes of the inequalities and methods
    of the class.

--- FME/F_M_Elimination/F_M_Elimination/Main.cpp
+++ FME/F_M_Elimination/F_M_Elimination/Main.cpp
@@ -6,6 +6,49 @@
 
 using namespace std;
 
+class inequality_index 
+{
+private :
+	int n;
+	int * index ;
+public:
+	inequality_index ()
+	{}
+	inequality_index (int total_inq, int pos )
+	{
+		n = total_inq;
+		index = new int [total_inq];
+		for (int i = 0; i<total_inq; i++){
+			index[i]= 0;
+			if (i == pos){
+				index[i] = 1;
+			}
+		}
+	}
+	~inequality_index ()
+	{ delete[] index;}
+
+	inequality_index* unite (inequality_index *set1,int total_inq){
+		for (int i = 0; i < total_inq; i++){
+			if((index [i] == 1)||(set1->index[i] == 1)){
+				set1->index [i] = 1;
+			}
+		}
+		return set1;
+	}
+	int calculate_index_size (inequality_index *a, inequality_index *b){
+		int total = 0;
+		inequality_index* tepm = a->unite(b, a->n);
+		for(int i =0; i < a->n; i++){
+			if (tepm->index[i] == 1){
+				total++;
+			}
+		}
+		return total;
+	}
+};
+
+
 /* Functions that initializes the matrix A and vector b  */
 int ** create_a(int col, int rows);
 int* create_b(int rows);
@@ -45,7 +88,11 @@ int main (int argc, char *argv[]){
 			cin>>b[i];
 		}
 	}
-
+	inequality_index * Q = new inequality_index [numRows];
+	for (int i =0; i < numRows; i++){
+		inequality_index x(numRows, i);
+		Q[i] = x;
+	}
 	int element_to_eliminate, total_to_eliminate;
 	cout << "Enter index of element to eliminate"<<'\n';
 	cin>> element_to_eliminate;
@@ -54,7 +101,7 @@ int main (int argc, char *argv[]){
 	long Start = GetTickCount();
 	for(int i = 0; i < total_to_eliminate; i++){
 		Calculating_new_system(A,b,element_to_eliminate,numRows,numVariables);    //all computations here
-		printResult(A, b, numRows, numVariables);
+		//printResult(A, b, numRows, numVariables);
 		element_to_eliminate ++;
 
 	}
