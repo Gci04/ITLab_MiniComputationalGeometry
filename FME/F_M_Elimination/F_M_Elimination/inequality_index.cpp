6e2f74f7b054f506f136c8d958a5519a10254a8e
Gci04 <gcinzoe04@gmail.com>
2016-03-18 12:54:21 +0300

    inequality index class modification

--- FME/F_M_Elimination/F_M_Elimination/inequality_index.cpp
+++ FME/F_M_Elimination/F_M_Elimination/inequality_index.cpp
@@ -1,12 +1,10 @@
 #include "inequality_index.h"
 
 inequality_index :: inequality_index(){
-		index = 0;
 	}
 inequality_index::inequality_index(const inequality_index &a, const inequality_index &b){
-	n = a.n;
-		index = new int [n];
-		for (int i = 0; i < n; i++){
+	
+	for (int i = 0; i < a.index.size(); i++){
 			if((b.index [i] == 1)||(a.index[i] == 1)){
 				index [i] = 1;
 			}
@@ -15,57 +13,49 @@ inequality_index::inequality_index(const inequality_index &a, const inequality_i
 		}
 	}
 inequality_index :: inequality_index(int total_inq, int pos ){
-	n = total_inq;
-		index = new int [total_inq];
 		for (int i = 0; i<total_inq; i++){
-			index[i]= 0;
 			if (i == pos){
-				index[i] = 1;
+				index.push_back(1);
 			}
+			else
+				index.push_back(0);
 		}
 	}
 inequality_index :: ~inequality_index() 
-{ delete[] index;}
+{}
 inequality_index * inequality_index :: unite (inequality_index *set1,int total_inq){
-	for (int i = 0; i < total_inq; i++){
+	inequality_index * temp;
+	for (int i = 0; i <index.size(); i++){
 			if((index [i] == 1)||(set1->index[i] == 1)){
-				set1->index [i] = 1;
+				temp->index [i] = 1;
 			}
+			else 
+				temp->index[i] =0;
 		}
-		return set1;
+		return temp;
 	}
-int inequality_index:: size (){
-		int total = 0;
-		for(int i =0; i < n; i++){
-			if (index[i] == 1){
-				total++;
-			}
-		}
-		return total;
+int inequality_index:: size ()const{
+		return index.size();
 	}
-bool inequality_index :: operator!=(inequality_index &A){
-	if (A.size() == size())
-			return false;
-		else 
-			return true;
-	}
-bool inequality_index :: is_subset (inequality_index &A){
-	bool test;
-	for (int i=0; i<n; i++){
-		if (index[i] != A.index[i]){
-			test = true;
+bool inequality_index :: operator!=(inequality_index &A) const{
+	for (int i = 0; i<index.size(); i++){
+		if (A.index[i] != index[i])
 			return false;
 		}
-		else
-			test = true;
-	}
-	return test;
+	return true;
+}
+bool inequality_index :: is_subset (inequality_index &A) const{
+	for (int i=0; i<index.size(); i++){
+		if (index[i] != A.index[i])
+			return false;
+		}
+	return true;
 }
 void  ineq_collection :: add_inequality(inequality S){
 	arr_inequality.push_back(S);
-	numInequalities++;
 }
-void  ineq_collection :: remove_inequality(inequality_index R){
-	int temp =0;
+void  ineq_collection :: remove_inequality(int idx){
+	arr_inequality[idx] = arr_inequality.back(); 
+	arr_inequality.pop_back();
 	
 }
--- FME/F_M_Elimination/F_M_Elimination/inequality_index.h
+++ FME/F_M_Elimination/F_M_Elimination/inequality_index.h
@@ -3,17 +3,16 @@
 class inequality_index 
 {
 private :
-	int n;
-	int * index ;
+	vector <int>index ;
 public:
 	inequality_index ();
 	inequality_index (const inequality_index &a, const inequality_index &b) ;
 	inequality_index (int total_inq, int pos );
 	~inequality_index ();
 	inequality_index* unite (inequality_index *set1,int total_inq);
-	int size ();
-	bool operator != (inequality_index &A);
-	bool is_subset (inequality_index &A);
+	int size () const;
+	bool operator != (inequality_index &A) const;
+	bool is_subset (inequality_index &A) const;
 };
 
 struct inequality {
@@ -27,7 +26,7 @@ public:
 	int numInequalities;
 
 	void add_inequality (inequality S);
-	void remove_inequality(inequality_index R);
+	void remove_inequality(int idx);
 	friend void create_linear_system(ineq_collection &A, int col, int rows);
 	friend void create_system_manualy(ineq_collection &A, int col, int rows);
 	friend void printResult(ineq_collection &K , int numrows, int numcols);
